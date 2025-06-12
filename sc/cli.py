254745c9286340a6da94b9172e2cf0dd827e0a75
Ivan Willig <iwillig@gmail.com>
2025-06-11 20:20:33 -0400

    Add a few more tools, rename groups to teams.

--- sc/cli.py
+++ sc/cli.py
@@ -1,17 +1,23 @@
 import click
+from sc.commands.teams import team
+from sc.commands.iteration import iteration
+from sc.commands.search import search
 
 @click.group()
 @click.version_option()
 def cli():
-    """SC - Command line tool."""
+    """SC - Shortcut Command Line Interface.
+
+    A command-line tool for interacting with Shortcut project management.
+
+    Set your SHORTCUT_API_TOKEN environment variable before using.
+    """
     pass
 
-@cli.command()
-@click.argument('name')
-@click.option('--greeting', '-g', default='Hello', help='Greeting to use')
-def greet(name, greeting):
-    """Greet someone."""
-    click.echo(f'{greeting}, {name}!')
+# Add command groups
+cli.add_command(team)
+cli.add_command(iteration)
+cli.add_command(search)
 
 if __name__ == '__main__':
     cli()
